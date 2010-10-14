9c77186e09f44018476f7c7aed2b3eba0e993658
Andy Denmark <denmark+github@gmail.com>
2010-10-13 17:31:55 -0700

    centralize whitespace management while building self._markdown and fixed the <h\d /> tag generation

--- src/html2markdown.py
+++ src/html2markdown.py
@@ -2,6 +2,7 @@
 
 from HTMLParser import HTMLParser
 
+import re
 import os
 import sys
 import string
@@ -23,27 +24,35 @@ class Html2MarkdownParser(HTMLParser):
         # FIXME: special characters
         HTMLParser.__init__(self)
 
+    def _append_to_markdown(self, new_markdown):
+        if len(self._markdown) > 1:
+            if re.match('\s', self._markdown[-1:]):
+                self._markdown += new_markdown
+            else:
+                self._markdown += ' ' + new_markdown
+        else:
+            self._markdown += new_markdown
+
     # <a />
     def handle_start_a(self, attrs):
         self._tag_attr_data = dict(attrs)
 
     def handle_end_a(self):
-        # FIXME: generalize this?
-        if len(self._markdown) and self._markdown[-1] != os.linesep: self._markdown += ' '
-
-        self._markdown += '[' + self._handled_tag_body_data + ']'
-        self._markdown += '(' + self._tag_attr_data.get('href')
+        a_tag = ''
+        a_tag += '[' + self._handled_tag_body_data + ']'
+        a_tag += '(' + self._tag_attr_data.get('href')
 
         title = self._tag_attr_data.get('title')
         if title:
-            self._markdown += ' "' + title + '") '
+            a_tag += ' "' + title + '") '
         else:
-            self._markdown += ') '
+            a_tag += ') '
+        self._append_to_markdown(a_tag)
 
     # <b />
     def handle_end_b(self):
         self._handled_tag_body_data = self._handled_tag_body_data.replace(os.linesep, ' ')
-        self._markdown += '*' + self._handled_tag_body_data + '*\n'
+        self._append_to_markdown('*' + self._handled_tag_body_data + '*')
 
     # <blockquote />
     def handle_end_blockquote(self):
@@ -51,59 +60,64 @@ class Html2MarkdownParser(HTMLParser):
 
         for blockquote_line in blockquote_body:
             blockquote_line = blockquote_line.strip()
-            self._markdown += '> ' + blockquote_line + os.linesep
+            self._append_to_markdown('> ' + blockquote_line + os.linesep)
 
     # <em />
     def handle_end_em(self):
         self._handled_tag_body_data = self._handled_tag_body_data.replace(os.linesep, ' ')
-        self._markdown += '*' + self._handled_tag_body_data + '*\n'
+        self._append_to_markdown('*' + self._handled_tag_body_data + '*')
 
     # <h1 />
     def handle_end_h1(self):
         self._handled_tag_body_data = self._handled_tag_body_data.replace(os.linesep, ' ')
-        self._markdown += '# ' + self._handled_tag_body_data + ' #\n'
+        self._append_to_markdown('# ' + self._handled_tag_body_data + ' #' + os.linesep)
 
     # <h2 />
     def handle_end_h2(self):
         self._handled_tag_body_data = self._handled_tag_body_data.replace(os.linesep, ' ')
-        self._markdown += '# ' + self._handled_tag_body_data + ' #\n'
+        self._append_to_markdown('## ' + self._handled_tag_body_data + ' ##' + os.linesep)
 
     # <h3 />
     def handle_end_h3(self):
         self._handled_tag_body_data = self._handled_tag_body_data.replace(os.linesep, ' ')
-        self._markdown += '# ' + self._handled_tag_body_data + ' #\n'
+        self._append_to_markdown('### ' + self._handled_tag_body_data + ' ###' + os.linesep)
 
     # <h4 />
     def handle_end_h4(self):
         self._handled_tag_body_data = self._handled_tag_body_data.replace(os.linesep, ' ')
-        self._markdown += '# ' + self._handled_tag_body_data + ' #\n'
+        self._append_to_markdown('#### ' + self._handled_tag_body_data + ' ####' + os.linesep)
 
     # <h5 />
     def handle_end_h5(self):
         self._handled_tag_body_data = self._handled_tag_body_data.replace(os.linesep, ' ')
-        self._markdown += '# ' + self._handled_tag_body_data + ' #\n'
+        self._append_to_markdown('##### ' + self._handled_tag_body_data + ' #####' + os.linesep)
 
     # <h6 />
     def handle_end_h6(self):
         self._handled_tag_body_data = self._handled_tag_body_data.replace(os.linesep, ' ')
-        self._markdown += '# ' + self._handled_tag_body_data + ' #\n'
+        self._append_to_markdown('###### ' + self._handled_tag_body_data + ' ######' + os.linesep)
 
     # <hr />
     def handle_start_hr(self, attrs):
-        self._markdown += '* * *\n'
+        self._append_to_markdown('* * *' + os.linesep)
 
     # <li />
     def handle_end_li(self):
         if len(self._tag_stack):
             if self._tag_stack[-1] == 'ol':
-                self._markdown += '1.    ' + self._handled_tag_body_data + '\n'
+                self._append_to_markdown('1.    ' + self._handled_tag_body_data + os.linesep)
             elif self._tag_stack[-1] == 'ul':
-                self._markdown += '*    ' + self._handled_tag_body_data + '\n'
+                self._append_to_markdown('*    ' + self._handled_tag_body_data + os.linesep)
 
     # <p />
     def handle_start_p(self, attrs):
-        if len(self._markdown) > 1 and self._markdown[-2:] != '%s%s' % (os.linesep, os.linesep):
-            self._markdown += os.linesep
+        if len(self._markdown) > 1:
+            if self._markdown[-2:] == '%s%s' % (os.linesep, os.linesep):
+                pass
+            elif self._markdown[-1:] == os.linesep:
+                self._markdown += os.linesep
+            else:
+                self._markdown += os.linesep + os.linesep
 
     def handle_end_p(self):
         self._markdown += '%s%s' % (os.linesep, os.linesep)
@@ -113,12 +127,12 @@ class Html2MarkdownParser(HTMLParser):
         code_lines = self._handled_tag_body_data.split(os.linesep)
         for code_line in code_lines:
             code_line = code_line.strip()
-            self._markdown += '    ' + code_line + os.linesep
+            self._append_to_markdown('    ' + code_line + os.linesep)
 
     # <strong />
     def handle_end_strong(self):
         self._handled_tag_body_data = self._handled_tag_body_data.replace(os.linesep, ' ')
-        self._markdown += '**' + self._handled_tag_body_data + '**\n'
+        self._append_to_markdown('**' + self._handled_tag_body_data + '**')
 
     ## ###
     def handle_starttag(self, tag, attrs):
@@ -147,13 +161,14 @@ class Html2MarkdownParser(HTMLParser):
         if len(self._tag_stack) and self._tag_stack[-1] not in ['p']:
             self._handled_tag_body_data += data
         else:
-            self._markdown += data
+            self._append_to_markdown(data)
 
     def get_markdown(self):
         return self._markdown.rstrip() + '\n'
 
 def main():
     p = Html2MarkdownParser()
+    buf = sys.stdin.read()
     p.feed(buf)
     p.close()
     print p.get_markdown()
